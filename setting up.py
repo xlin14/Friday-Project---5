8ff6e5265cb8ac6e6ddf2ae99889f3e1aaaa3968
xlin14 <xlin4414@gmail.com>
2025-10-05 20:43:33 -0500

    database info

--- setting up.py
+++ setting up.py
@@ -2,3 +2,21 @@ import tkinter as tk
 from tkinter import messagebox
 import sqlite3
 import re
+
+def setup_database():
+    """Connects to the SQLite database and creates the customers table if it doesn't exist."""
+    conn = sqlite3.connect('customer_data.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS customers (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            birthday TEXT,
+            email TEXT,
+            phone TEXT,
+            address TEXT,
+            contact_method TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
